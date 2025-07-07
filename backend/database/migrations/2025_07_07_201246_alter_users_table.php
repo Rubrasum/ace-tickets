f1f146b77b483a16c7268ad42958e6e48c1a9c6e
Rubrasum <josephbetbeze@gmail.com>
2025-07-07 16:28:21 -0400

    Added the migrations for a standard set of data per basic requirements for a ticketing system with employee management.

---
+++ backend/database/migrations/2025_07_07_201246_alter_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user');
+            $table->foreignId('device_id')->nullable()->constrained()->nullOnDelete();
+            $table->timestamp('last_login_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['device_id']);
+            $table->dropColumn(['role', 'device_id', 'last_login_at']);
+        });
+    }
+};
